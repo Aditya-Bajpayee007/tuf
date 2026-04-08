bb7bec53dba13b29042d1f0d8fbdc2e0a8778c2c
Aditya-Bajpayee007 <adityabajpayee7@gmail.com>
2026-04-09 03:23:40 +0530

    7

--- src/components/CalendarGrid.jsx
+++ src/components/CalendarGrid.jsx
@@ -10,7 +10,7 @@ const gridVariants = {
 
 export default function CalendarGrid({ monthDays, currentMonth, selection, handleDateClick, theme, slideCount, direction }) {
   return (
-    <div className="relative h-[300px] md:h-[420px]">
+    <div className="relative h-[300px] md:min-h-screen">
       <AnimatePresence mode="wait" custom={direction} initial={false}>
         <motion.div
           key={`slide-${slideCount}`}
